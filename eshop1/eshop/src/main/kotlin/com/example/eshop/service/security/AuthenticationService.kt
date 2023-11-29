f3fff505cdba4ef427461dcaf7a234c04802dbd2
Ivan.Zhirnov <ivan.zhirnov@fleetcor.ru>
2023-11-29 20:06:16 +0300

    add back auth

---
+++ eshop1/eshop/src/main/kotlin/com/example/eshop/service/security/AuthenticationService.kt
@@ -0,0 +1,71 @@
+package com.example.eshop.service.security
+
+import com.example.eshop.config.JwtProperties
+import com.example.eshop.dto.auth.AuthenticationRequest
+import com.example.eshop.dto.auth.AuthenticationResponse
+import com.example.eshop.repository.security.RefreshTokenRepository
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class AuthenticationService(
+    private val authManager: AuthenticationManager,
+    private val userDetailsService: CustomUserDetailsService,
+    private val tokenService: TokenService,
+    private val jwtProperties: JwtProperties,
+    private val refreshTokenRepository: RefreshTokenRepository
+) {
+    fun authentication(authenticationRequest: AuthenticationRequest): AuthenticationResponse {
+        authManager.authenticate(
+            UsernamePasswordAuthenticationToken(
+                authenticationRequest.username,
+                authenticationRequest.password
+            )
+        )
+
+        val user = userDetailsService.loadUserByUsername(authenticationRequest.username)
+
+        val accessToken = createAccessToken(user)
+        val refreshToken = createRefreshToken(user)
+
+        refreshTokenRepository.save(refreshToken, user)
+
+        return AuthenticationResponse(
+            accessToken = accessToken,
+            refreshToken = refreshToken
+        )
+    }
+
+    fun refreshAccessToken(refreshToken: String): String? {
+        val extractedEmail = tokenService.extractUsername(refreshToken)
+
+        return extractedEmail?.let { email ->
+            val currentUserDetails = userDetailsService.loadUserByUsername(email)
+            val refreshTokenUserDetails = refreshTokenRepository.findUserDetailsByToken(refreshToken)
+
+            if (!tokenService.isExpired(refreshToken) && refreshTokenUserDetails?.username == currentUserDetails.username)
+                createAccessToken(currentUserDetails)
+            else
+                null
+        }
+    }
+
+    private fun createAccessToken(user: UserDetails) = tokenService.generate(
+        userDetails = user,
+        expirationDate = getAccessTokenExpiration()
+    )
+
+    private fun createRefreshToken(user: UserDetails) = tokenService.generate(
+        userDetails = user,
+        expirationDate = getRefreshTokenExpiration()
+    )
+
+    private fun getAccessTokenExpiration(): Date =
+        Date(System.currentTimeMillis() + jwtProperties.accessTokenExpiration)
+
+    private fun getRefreshTokenExpiration(): Date =
+        Date(System.currentTimeMillis() + jwtProperties.refreshTokenExpiration)
+}
